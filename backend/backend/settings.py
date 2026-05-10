ed900d3319a1483e70d86b64c6ea1c9c8b20d373
Yuri Mauricio <sangeandyasha04@gmail.com>
2026-05-11 00:25:59 +0800

    add token blacklist

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -21,7 +21,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'rest_framework',
     'corsheaders',
-
+    'rest_framework_simplejwt.token_blacklist',
     'diary_project.apps.DiaryProjectConfig',
     'accounts.apps.AccountsConfig',
 ]
